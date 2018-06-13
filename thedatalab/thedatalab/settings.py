958222705443a7b26cc92677d70e9f3d809ef7e9
Seb Bacon <seb.bacon@gmail.com>
2018-06-13 09:05:12 +0100

    Add some wireframe-like styling

--- thedatalab/thedatalab/settings.py
+++ thedatalab/thedatalab/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '../static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'tagulous',
     'imagefit',
+    'compressor',
     'frontend'
 ]
 
