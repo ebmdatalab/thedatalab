a24052e462fc4e825799bb4033b28243b6763ecb
Seb Bacon <seb.bacon@gmail.com>
2018-06-15 08:55:29 +0100

    Fix supervisord conf for staging environment

--- deploy/run-thedatalab_staging.sh
+++ deploy/run-thedatalab_staging.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cd /var/www/thedatalab_staging/thedatalab
+cd /var/www/thedatalab_staging/thedatalab/thedatalab
 
-. /etc/profile.d/thedatalab_staging.sh && exec ../../venv/bin/gunicorn frontend.wsgi -c ../deploy/gunicorn-thedatalab_staging.conf.py
+. /etc/profile.d/thedatalab_staging.sh && exec ../../venv/bin/gunicorn thedatalab.wsgi -c ../deploy/gunicorn-thedatalab_staging.conf.py
