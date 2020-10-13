036dcdc26dd7c1c2ee5aa6e6010f57be0723e758
Tom Ward <tomward@fmail.co.uk>
2020-10-13 12:15:09 +0100

    Add robots.txt

--- thedatalab/urls.py
+++ thedatalab/urls.py
@@ -61,6 +61,11 @@ urlpatterns = [
     path('projects/<slug:slug>/', views.project_view, name='project_view'),
     path('topics/<slug:slug>/', views.topic_view, name='topic_index'),
     
+    path(
+        r"robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
+
     path('', views.home_view, name='home'),
 
     re_path(r'^(.*)$', views.page_view, name='page_view'),
